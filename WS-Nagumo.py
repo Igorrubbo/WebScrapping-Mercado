73de877cf027e9af92ca7508e7ed9caf04c3b453
Igor <igor_rubbo@hotmail.com>
2022-02-08 17:31:25 -0300

    Options alterado e dados do site trabalhados

--- WS-Nagumo.py
+++ WS-Nagumo.py
@@ -6,13 +6,14 @@ import os
 import time
 
 # Instantiate an Options object and add the "--headless" argument
-opts = Options()
+#opts = Options()
+
+opts = webdriver.ChromeOptions()
 opts.add_argument(" --headless")
+opts.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 # Set the location of the webdriver and instantiate a webdriver
 chrome_driver = Service(r'C:\Program Files (x86)\Chromedriver\chromedriver.exe')
-#driver = webdriver.Chrome(service=chrome_driver, options = opts)
-#chrome_driver = r'C:\Program Files (x86)\Chromedriver\chromedriver.exe'
 driver = webdriver.Chrome(options = opts, service = chrome_driver)
 
 # Load the HTML page and pause so the page has time to load
@@ -22,13 +23,17 @@ time.sleep(1)
 
 # Put the page source into a variable and create a BS object from it
 soup_file = driver.page_source
-site_nagumo = BeautifulSoup(soup_file)
+site_nagumo = BeautifulSoup(soup_file, 'html.parser')
 main = site_nagumo.find(class_ = 'flex fww fww-products')
-titulo = main.find(class_ = 'list-product-link')
-print(titulo.get_text)
-preco_total = main.find(class_='area-bloco-preco bloco-preco pr-0')
-print(preco_total.text)
 
-# Load and print the title and the text of the <div>
-#print(soup.title.get_text())
-#print(soup.find(class_='area-bloco-preco bloco-preco pr-0').get_text())
+titulo_auxiliar = main.find(class_ = 'txt-desc-product-itemtext-muted txt-desc-product-item')
+titulo_nagumo = titulo_auxiliar.find(class_ = 'list-product-link')
+print(titulo_nagumo.text)
+
+link_nagumo = main.find(class_ = 'list-product-link')
+novo_link = link_nagumo.text, link_nagumo['href']
+print((link_nagumo.text, link_nagumo['href'])[1])
+
+preco_total_nagumo = main.find(class_='area-bloco-preco bloco-preco pr-0')
+print(preco_total_nagumo.text)
+
